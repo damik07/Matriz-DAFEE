44ae86d8004f13b80d84929006f0e683562a2756
damik07 <96784561+damik07@users.noreply.github.com>
2022-12-13 13:14:12 -0300

    2 files modified, 1 deleted

--- src/app/Componentes/Nacionales/vistas/comp-integ/comp-integ.component.ts
+++ src/app/Componentes/Nacionales/vistas/comp-integ/comp-integ.component.ts
@@ -10,12 +10,23 @@ import { IntegracionService } from '../../../../Servicios/integracion.service';
 export class CompIntegComponent implements OnInit {
 
   dataInteg?:any[];
+  datosFiltrados?:any[];
+  filtroFechas:Date;
+  
 
   constructor(private router:Router, private intService:IntegracionService) {
     this.dataInteg = this.intService.integracion;
     }
 
   ngOnInit() {
+
+    
+  }
+
+  filterData1(){     
+    console.log(this.filterData1)
+    // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
+  this.datosFiltrados = this.dataInteg.filter(dato => dato.fecha >= this.filterData1.arguments && dato.fecha <= this.filterData1.arguments);  
   }
 
 }
