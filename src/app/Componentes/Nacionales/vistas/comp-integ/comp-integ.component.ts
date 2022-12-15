b89edf6f698f29278ebb64bacb76d012303ca0c1
damik07 <96784561+damik07@users.noreply.github.com>
2022-12-15 10:00:31 -0300

    filtros concatenados e identificados

--- src/app/Componentes/Nacionales/vistas/comp-integ/comp-integ.component.ts
+++ src/app/Componentes/Nacionales/vistas/comp-integ/comp-integ.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IntegracionService } from '../../../../Servicios/integracion.service';
+import { groupBy } from 'rxjs/operators';
 
 @Component({
   selector: 'app-comp-integ',
   templateUrl: './comp-integ.component.html',
   styleUrls: ['./comp-integ.component.css']
 })
+
 export class CompIntegComponent implements OnInit {
 
   dataInteg?:any[];
   datosFiltrados?:any[];
-  
-  
-  
+  item:any;
+    
 
   constructor(private router:Router, private intService:IntegracionService) {
     this.dataInteg = this.intService.integracion;
@@ -25,9 +26,30 @@ export class CompIntegComponent implements OnInit {
 
   filterData1(formData){           
     // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
-    this.datosFiltrados = this.dataInteg.filter(dato => dato.fecha >= formData.startDate1 && dato.fecha <= formData.endDate1);
-    
+    const datosFiltrados1 = this.dataInteg.filter(dato => 
+      dato.fecha >= formData.startDate1 && dato.fecha <= formData.endDate1);
+    const datosFiltrados2 = this.dataInteg.filter(dato =>
+      dato.fecha >= formData.startDate2 && dato.fecha <= formData.endDate2);
+      
+      const dFiltrado1 = datosFiltrados1.map(obj => ({
+        ...obj,
+        filtro: "filtro1"
+      }));
+      
+      const dfiltrado2 = datosFiltrados2.map(obj => ({
+        ...obj,
+        filtro: "filtro2"
+      }));
+
+
+      this.datosFiltrados = datosFiltrados1.concat(datosFiltrados2);
+      console.log(this.datosFiltrados);
     
   }
+
+  filtroByFiltros(filtro:any){
+    return this.item.filtro === filtro
+  }
+
 
 }
