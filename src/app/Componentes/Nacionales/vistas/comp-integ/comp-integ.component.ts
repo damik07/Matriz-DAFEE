f4cd2752d0223d495e7f1c5ca9334844802c0cc0
damik07 <96784561+damik07@users.noreply.github.com>
2022-12-29 09:26:18 -0300

    2 files modified

--- src/app/Componentes/Nacionales/vistas/comp-integ/comp-integ.component.ts
+++ src/app/Componentes/Nacionales/vistas/comp-integ/comp-integ.component.ts
@@ -17,65 +17,73 @@ export class CompIntegComponent implements OnInit {
   datosFiltrados?:any[];
   item:any;
   sumary?:any[];
+  total1:any;
+  total2:any;
     
 
   constructor(private router:Router, private intService:IntegracionService) {
     this.dataInteg = this.intService.integracion;
+  }
+
+    ngOnInit() {
+         
     }
 
-  ngOnInit() {
+    filterData1(formData){           
+      // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
+      const datosFiltrados1 = this.dataInteg.filter(dato => 
+        dato.fecha >= formData.startDate1 && dato.fecha <= formData.endDate1);
+      const datosFiltrados2 = this.dataInteg.filter(dato =>
+        dato.fecha >= formData.startDate2 && dato.fecha <= formData.endDate2);
         
-  }
+        const dFiltrado1 = datosFiltrados1.map(obj => ({
+          ...obj,
+          importe1: obj.importe,
+          ...obj,
+          importe2: 0
+        }));
+        
+        const dfiltrado2 = datosFiltrados2.map(obj => ({
+          ...obj,
+          importe1: 0,
+          ...obj,
+          importe2: obj.importe
+        }));
 
-  filterData1(formData){           
-    // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
-    const datosFiltrados1 = this.dataInteg.filter(dato => 
-      dato.fecha >= formData.startDate1 && dato.fecha <= formData.endDate1);
-    const datosFiltrados2 = this.dataInteg.filter(dato =>
-      dato.fecha >= formData.startDate2 && dato.fecha <= formData.endDate2);
-      
-      const dFiltrado1 = datosFiltrados1.map(obj => ({
-        ...obj,
-        importe1: obj.importe,
-        ...obj,
-        importe2: 0
-      }));
-      
-      const dfiltrado2 = datosFiltrados2.map(obj => ({
-        ...obj,
-        importe1: 0,
-        ...obj,
-        importe2: obj.importe
-      }));
+        this.datosFiltrados = dFiltrado1.concat(dfiltrado2);
+        console.log(this.datosFiltrados);
+        
+        this.sumary = this.datosFiltrados.reduce((acc, item) => {
+          const existingItem = acc.find(i => i.descripcion === item.descripcion);
+              if (existingItem) {
+              existingItem.importe += item.importe;
+              existingItem.importe1 += item.importe1;
+              existingItem.importe2 += item.importe2;
+            } else {
+              acc.push({descripcion: item.descripcion, importe: item.importe, importe1: item.importe1, importe2: item.importe2});
+            }
+            return acc;
+          }, []);
+        console.log(this.sumary);
 
-      this.datosFiltrados = dFiltrado1.concat(dfiltrado2);
-      console.log(this.datosFiltrados);
+        this.total1 = this.sumary.reduce((acc, item) => acc + item.importe1,0);
+        this.total2 = this.sumary.reduce((acc, item) => acc + item.importe2,0);
       
-      this.sumary = this.datosFiltrados.reduce((acc, item) => {
-        const existingItem = acc.find(i => i.descripcion === item.descripcion);
-            if (existingItem) {
-            existingItem.importe += item.importe;
-            existingItem.importe1 += item.importe1;
-            existingItem.importe2 += item.importe2;
-          } else {
-            acc.push({descripcion: item.descripcion, importe: item.importe, importe1: item.importe1, importe2: item.importe2});
-          }
-          return acc;
-        }, []);
-      console.log(this.sumary);
+    };
+
     
-  };
 
-  name = 'ExcelSheet.xlsx';
-  exportToExcel(): void {
-    let element = document.getElementById('comp-integracion');
-    const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
 
-    const book: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(book, worksheet, 'Hoja1');
+    name = 'ExcelSheet.xlsx';
+    exportToExcel(): void {
+      let element = document.getElementById('comp-integracion');
+      const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
 
-    XLSX.writeFile(book, this.name);
-  }
+      const book: XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(book, worksheet, 'Hoja1');
 
+      XLSX.writeFile(book, this.name);
+    }
+  
 
 }
