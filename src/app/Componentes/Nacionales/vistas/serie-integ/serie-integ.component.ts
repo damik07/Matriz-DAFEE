a44e2a05ff212b7dd433337a264cb46f914ec698
damik07 <96784561+damik07@users.noreply.github.com>
2022-12-30 11:50:03 -0300

    2 files modified

--- src/app/Componentes/Nacionales/vistas/serie-integ/serie-integ.component.ts
+++ src/app/Componentes/Nacionales/vistas/serie-integ/serie-integ.component.ts
@@ -10,11 +10,29 @@ import { IntegracionService } from '../../../../Servicios/integracion.service';
 export class SerieIntegComponent implements OnInit {
 
   dataInteg?:any[];
+  cuentas:any[];
 
   constructor(private router:Router, private intService:IntegracionService) {
-    this.dataInteg = this.intService.integracion; }
-
-  ngOnInit() {
+    this.dataInteg = this.intService.integracion; 
+    this.cuentas = this.dataInteg.reduce((acc, item) => {
+      const existingItem = acc.find(i => i.descripcion === item.descripcion);
+          if (existingItem) {
+          existingItem.descripcion;
+        } else {
+          acc.push({descripcion: item.descripcion});
+        }
+        return acc;
+      }, []);
+      
+  
   }
 
+  ngOnInit() {
+
+  };
+
+ 
+
+
+  
 }
