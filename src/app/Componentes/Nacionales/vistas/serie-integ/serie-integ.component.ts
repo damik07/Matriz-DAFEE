674866b6da08e7a56dc71263ed4285421c3fa587
damik07 <96784561+damik07@users.noreply.github.com>
2023-01-02 12:28:05 -0300

    3 files modified

--- src/app/Componentes/Nacionales/vistas/serie-integ/serie-integ.component.ts
+++ src/app/Componentes/Nacionales/vistas/serie-integ/serie-integ.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { IntegracionService } from '../../../../Servicios/integracion.service';
+import Chart from 'chart.js/auto'
 
 @Component({
   selector: 'app-serie-integ',
@@ -11,6 +13,7 @@ export class SerieIntegComponent implements OnInit {
 
   dataInteg?:any[];
   cuentas:any[];
+  
 
   constructor(private router:Router, private intService:IntegracionService) {
     this.dataInteg = this.intService.integracion; 
@@ -31,7 +34,57 @@ export class SerieIntegComponent implements OnInit {
 
   };
 
- 
+  filterGrafico(formData){
+    const filtroFecha = this.dataInteg.filter(dato => 
+      dato.fecha >= formData.startDate1 && dato.fecha <= formData.endDate1);
+    const filtroCuenta = filtroFecha.filter(dato =>
+      dato.descripcion = formData.cuenta);
+
+    
+    
+    console.log(filtroCuenta);
+
+
+    // Inicializa el gráfico
+    let chart = new Chart('chart', {
+    type: 'bar',
+    data: {
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+      datasets: [
+        {
+          label: 'Año 2020',
+          data: [100, 200, 300, 400, 500, 600],
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        },
+        {
+          label: 'Año 2021',
+          data: [200, 400, 600, 800, 1000, 1200],
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1
+        }
+      ]
+    },
+    //options: {    scales: {      yAxes: [{        ticks: {          beginAtZero: true        }      }]    }  }
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Bar Chart'
+        }
+      }
+    },
+    });
+
+
+  
+  }
 
 
   
