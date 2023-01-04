fb0d32ece017e35fb5cb9497e596b2c51d1d6acf
damik07 <96784561+damik07@users.noreply.github.com>
2023-01-04 13:03:40 -0300

    1 file added

---
+++ src/app/Servicios/ipc.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class IpcService {
+
+  ipc?:any=[{
+    "fecha": "2022-11-01",
+    "concepto": "IPC Indec",
+    "valor": 560.757205
+
+  },
+  {
+    "fecha": "2023-11-01",
+    "concepto": "IPC Indec",
+    "valor": 1079.2787
+
+  }
+  ]
+
+}
