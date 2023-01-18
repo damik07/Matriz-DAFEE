6d35b46dac6322e6dd8cac77d4de50f4eca19308
damik07 <96784561+damik07@users.noreply.github.com>
2023-01-18 10:34:04 -0300

    1 file modified

--- src/app/Componentes/plantaPersonal/vistas/pl-comp-mens/pl-comp-mens.component.ts
+++ src/app/Componentes/plantaPersonal/vistas/pl-comp-mens/pl-comp-mens.component.ts
@@ -23,58 +23,58 @@ export class PlCompMensComponent implements OnInit {
   ngOnInit() {
   }
 
-  filterData1(formData){ 
-// Filtrar los datos del servicio según el rango de fechas especificado en el formulario
-const datosFiltrados1 = this.dataPlanta.filter(dato => 
-  new Date(dato.fecha).getMonth() === new Date(formData.startDate1).getMonth()
-);
-const dfiltro1 = datosFiltrados1.filter(dato =>
-  new Date(dato.fecha).getFullYear() === new Date(formData.startDate1).getFullYear()
-);
+  filterData1(formData) {
+    // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
+    const datosFiltrados1 = this.dataPlanta.filter(dato =>
+      new Date(dato.fecha).getMonth() === new Date(formData.startDate1).getMonth()
+    );
+    const dfiltro1 = datosFiltrados1.filter(dato =>
+      new Date(dato.fecha).getFullYear() === new Date(formData.startDate1).getFullYear()
+    );
 
-const datosFiltrados2 = this.dataPlanta.filter(dato => 
-  new Date(dato.fecha).getMonth() === new Date(formData.endDate1).getMonth()
-);
-const dfiltro2 = datosFiltrados2.filter(dato =>
-  new Date(dato.fecha).getFullYear() === new Date(formData.endDate1).getFullYear()
-);
-  
-  const dFiltrado1 = dfiltro1.map(obj => ({
-    ...obj,
-    cantidad1: obj.cantidad,
-    ...obj,
-    cantidad2: 0
-  }));
-  
-  const dfiltrado2 = dfiltro2.map(obj => ({
-    ...obj,
-    cantidad1: 0,
-    ...obj,
-    cantidad2: obj.cantidad
-  }));
+    const datosFiltrados2 = this.dataPlanta.filter(dato =>
+      new Date(dato.fecha).getMonth() === new Date(formData.endDate1).getMonth()
+    );
+    const dfiltro2 = datosFiltrados2.filter(dato =>
+      new Date(dato.fecha).getFullYear() === new Date(formData.endDate1).getFullYear()
+    );
 
-  const datosFiltrados = dFiltrado1.concat(dfiltrado2);
-    
-  const planta = datosFiltrados.filter(e => e.tipo_planta === 'Permanente');
-  this.permanente = planta.reduce((acc, item) => {
-    
-    const existingItem = acc.find(i => i.escalafon === item.escalafon);
-        if (existingItem) {
+    const dFiltrado1 = dfiltro1.map(obj => ({
+      ...obj,
+      cantidad1: obj.cantidad,
+      ...obj,
+      cantidad2: 0
+    }));
+
+    const dfiltrado2 = dfiltro2.map(obj => ({
+      ...obj,
+      cantidad1: 0,
+      ...obj,
+      cantidad2: obj.cantidad
+    }));
+
+    const datosFiltrados = dFiltrado1.concat(dfiltrado2);
+
+    const plantaPer = datosFiltrados.filter(e => e.tipo_planta === 'Permanente');
+    this.permanente = plantaPer.reduce((acc, item) => {
+
+      const existingItem = acc.find(i => i.escalafon === item.escalafon);
+      if (existingItem) {
         existingItem.cantidad += item.cantidad;
         existingItem.cantidad1 += item.cantidad1;
         existingItem.cantidad2 += item.cantidad2;
       } else {
-        acc.push({escalafon: item.escalafon, cantidad: item.cantidad, cantidad1: item.cantidad1, cantidad2: item.cantidad2});
+        acc.push({ escalafon: item.escalafon, cantidad: item.cantidad, cantidad1: item.cantidad1, cantidad2: item.cantidad2 });
       }
       return acc;
     }, []);
-  
+
     console.log(this.permanente)
 
-  this.total1 = this.permanente.reduce((acc, item) => acc + item.cantidad1,0);
-  this.total2 = this.permanente.reduce((acc, item) => acc + item.cantidad2,0);
+    this.total1 = this.permanente.reduce((acc, item) => acc + item.cantidad1, 0);
+    this.total2 = this.permanente.reduce((acc, item) => acc + item.cantidad2, 0);
 
-};
+  };
 
   name = 'Comparativo Integracion.xlsx';
     exportToExcel(): void {
