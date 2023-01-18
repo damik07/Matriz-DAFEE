2ae23c809c9118d1975a90c7bd9d2435cef1d9c6
damik07 <96784561+damik07@users.noreply.github.com>
2023-01-18 12:43:12 -0300

    1 file modified

--- src/app/Componentes/plantaPersonal/vistas/pl-comp-mens/pl-comp-mens.component.ts
+++ src/app/Componentes/plantaPersonal/vistas/pl-comp-mens/pl-comp-mens.component.ts
@@ -3,166 +3,195 @@ import { Router } from '@angular/router';
 import * as XLSX from 'xlsx';
 import { PlantaDePersonalService } from '../../../../Servicios/planta-de-personal.service';
 
-
-
 @Component({
   selector: 'app-pl-comp-mens',
   templateUrl: './pl-comp-mens.component.html',
-  styleUrls: ['./pl-comp-mens.component.css']
+  styleUrls: ['./pl-comp-mens.component.css'],
 })
 export class PlCompMensComponent implements OnInit {
+  dataPlanta?: any[];
+  permanente?: any[];
+  total1Per: any;
+  total2Per: any;
+  temporario?: any[];
+  total1Tem: any;
+  total2Tem: any;
+  suplente?: any[];
+  total1Sup: any;
+  total2Sup: any;
+  headers = ['Planta Permanente', 'Planta Temporaria', 'Planta Suplente'];
 
-  dataPlanta?:any[];
-  permanente?:any[];
-  total1Per:any;
-  total2Per:any;
-  temporario?:any[];
-  total1Tem:any;
-  total2Tem:any;
-  suplente?:any[];
-  total1Sup:any;
-  total2Sup:any;
-  headers = ["Planta Permanente", "Planta Temporaria", "Planta Suplente"];
-  
-  
-  constructor(private router:Router, private plService:PlantaDePersonalService) { 
+  constructor(
+    private router: Router,
+    private plService: PlantaDePersonalService
+  ) {
     this.dataPlanta = this.plService.planta;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   filterData1(formData) {
     // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
-    const datosFiltrados1 = this.dataPlanta.filter(dato =>
-      new Date(dato.fecha).getMonth() === new Date(formData.startDate1).getMonth()
+    const datosFiltrados1 = this.dataPlanta.filter(
+      (dato) =>
+        new Date(dato.fecha).getMonth() ===
+        new Date(formData.startDate1).getMonth()
     );
-    const dfiltro1 = datosFiltrados1.filter(dato =>
-      new Date(dato.fecha).getFullYear() === new Date(formData.startDate1).getFullYear()
-    );
-
-    const datosFiltrados2 = this.dataPlanta.filter(dato =>
-      new Date(dato.fecha).getMonth() === new Date(formData.endDate1).getMonth()
-    );
-    const dfiltro2 = datosFiltrados2.filter(dato =>
-      new Date(dato.fecha).getFullYear() === new Date(formData.endDate1).getFullYear()
+    const dfiltro1 = datosFiltrados1.filter(
+      (dato) =>
+        new Date(dato.fecha).getFullYear() ===
+        new Date(formData.startDate1).getFullYear()
     );
 
-    const dFiltrado1 = dfiltro1.map(obj => ({
+    const datosFiltrados2 = this.dataPlanta.filter(
+      (dato) =>
+        new Date(dato.fecha).getMonth() ===
+        new Date(formData.endDate1).getMonth()
+    );
+    const dfiltro2 = datosFiltrados2.filter(
+      (dato) =>
+        new Date(dato.fecha).getFullYear() ===
+        new Date(formData.endDate1).getFullYear()
+    );
+
+    const dFiltrado1 = dfiltro1.map((obj) => ({
       ...obj,
       cantidad1: obj.cantidad,
       ...obj,
-      cantidad2: 0
+      cantidad2: 0,
     }));
 
-    const dfiltrado2 = dfiltro2.map(obj => ({
+    const dfiltrado2 = dfiltro2.map((obj) => ({
       ...obj,
       cantidad1: 0,
       ...obj,
-      cantidad2: obj.cantidad
+      cantidad2: obj.cantidad,
     }));
 
     const datosFiltrados = dFiltrado1.concat(dfiltrado2);
 
     // Filtro para personal permanente quitando el personal de las empresas que no consolidan
-    const plantaPer = datosFiltrados.filter(e => e.tipo_planta === 'Permanente' && e.tipo_organismo !== 'Empresas 2');
+    const plantaPer = datosFiltrados.filter(
+      (e) => e.tipo_planta === 'Permanente' && e.tipo_organismo !== 'Empresas 2'
+    );
     this.permanente = plantaPer.reduce((acc, item) => {
-
-      const existingItem = acc.find(i => i.escalafon === item.escalafon);
+      const existingItem = acc.find((i) => i.escalafon === item.escalafon);
       if (existingItem) {
         existingItem.cantidad += item.cantidad;
         existingItem.cantidad1 += item.cantidad1;
         existingItem.cantidad2 += item.cantidad2;
       } else {
-        acc.push({ escalafon: item.escalafon, cantidad: item.cantidad, cantidad1: item.cantidad1, cantidad2: item.cantidad2 });
+        acc.push({
+          escalafon: item.escalafon,
+          cantidad: item.cantidad,
+          cantidad1: item.cantidad1,
+          cantidad2: item.cantidad2,
+        });
       }
       return acc;
     }, []);
 
-    this.total1Per = this.permanente.reduce((acc, item) => acc + item.cantidad1, 0);
-    this.total2Per = this.permanente.reduce((acc, item) => acc + item.cantidad2, 0);
+    this.total1Per = this.permanente.reduce(
+      (acc, item) => acc + item.cantidad1,
+      0
+    );
+    this.total2Per = this.permanente.reduce(
+      (acc, item) => acc + item.cantidad2,
+      0
+    );
 
     // Filtro para personal temporario quitando el personal de las empresas que no consolidan
-    const plantaTem = datosFiltrados.filter(e => e.tipo_planta === 'Temporario' && e.tipo_organismo !== 'Empresas 2');
+    const plantaTem = datosFiltrados.filter(
+      (e) => e.tipo_planta === 'Temporario' && e.tipo_organismo !== 'Empresas 2'
+    );
     this.temporario = plantaTem.reduce((acc, item) => {
-
-      const existingItem = acc.find(i => i.escalafon === item.escalafon);
+      const existingItem = acc.find((i) => i.escalafon === item.escalafon);
       if (existingItem) {
         existingItem.cantidad += item.cantidad;
         existingItem.cantidad1 += item.cantidad1;
         existingItem.cantidad2 += item.cantidad2;
       } else {
-        acc.push({ escalafon: item.escalafon, cantidad: item.cantidad, cantidad1: item.cantidad1, cantidad2: item.cantidad2 });
+        acc.push({
+          escalafon: item.escalafon,
+          cantidad: item.cantidad,
+          cantidad1: item.cantidad1,
+          cantidad2: item.cantidad2,
+        });
       }
       return acc;
     }, []);
 
-    this.total1Tem = this.temporario.reduce((acc, item) => acc + item.cantidad1, 0);
-    this.total2Tem = this.temporario.reduce((acc, item) => acc + item.cantidad2, 0);
+    this.total1Tem = this.temporario.reduce(
+      (acc, item) => acc + item.cantidad1,
+      0
+    );
+    this.total2Tem = this.temporario.reduce(
+      (acc, item) => acc + item.cantidad2,
+      0
+    );
 
     // Filtro para personal suplente quitando el personal de las empresas que no consolidan
-    const plantaSup = datosFiltrados.filter(e => e.tipo_planta === 'Suplente' && e.tipo_organismo !== 'Empresas 2');
+    const plantaSup = datosFiltrados.filter(
+      (e) => e.tipo_planta === 'Suplente' && e.tipo_organismo !== 'Empresas 2'
+    );
     this.suplente = plantaSup.reduce((acc, item) => {
-
-      const existingItem = acc.find(i => i.escalafon === item.escalafon);
+      const existingItem = acc.find((i) => i.escalafon === item.escalafon);
       if (existingItem) {
         existingItem.cantidad += item.cantidad;
         existingItem.cantidad1 += item.cantidad1;
         existingItem.cantidad2 += item.cantidad2;
       } else {
-        acc.push({ escalafon: item.escalafon, cantidad: item.cantidad, cantidad1: item.cantidad1, cantidad2: item.cantidad2 });
+        acc.push({
+          escalafon: item.escalafon,
+          cantidad: item.cantidad,
+          cantidad1: item.cantidad1,
+          cantidad2: item.cantidad2,
+        });
       }
       return acc;
     }, []);
 
-    this.total1Sup = this.suplente.reduce((acc, item) => acc + item.cantidad1, 0);
-    this.total2Sup = this.suplente.reduce((acc, item) => acc + item.cantidad2, 0);
+    this.total1Sup = this.suplente.reduce(
+      (acc, item) => acc + item.cantidad1,
+      0
+    );
+    this.total2Sup = this.suplente.reduce(
+      (acc, item) => acc + item.cantidad2,
+      0
+    );
+  }
 
-  };
-
-  name = 'Comparativo Planta de Personal.xlsx';
-    exportToExcel(): void {
-      let element = document.getElementById('comp-pl-mens');
-      const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
-
-      const book: XLSX.WorkBook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(book, worksheet, 'Hoja1');
-
-      XLSX.writeFile(book, this.name);
+  multiTable(): void {
+    /* creación de función que agrega fila */
+    function create_gap_rows(ws, nrows) {
+      var ref = XLSX.utils.decode_range(ws['!ref']); // busca rango original
+      ref.e.r += nrows; // agrega una fila al final
+      ws['!ref'] = XLSX.utils.encode_range(ref); // reasigna fila
     }
 
-    multiTable(): void {
-      
-    
-      /* Callback invoked when the button is clicked */
-      //const xport = React.useCallback(async () => {
-        /* This function creates gap rows */
-        function create_gap_rows(ws, nrows) {
-          var ref = XLSX.utils.decode_range(ws["!ref"]);       // get original range
-          ref.e.r += nrows;                                    // add to ending row
-          ws["!ref"] = XLSX.utils.encode_range(ref);           // reassign row
-        }
-    
-        /* first table */
-        const ws = XLSX.utils.aoa_to_sheet([[this.headers[0]]]);
-        XLSX.utils.sheet_add_dom(ws, document.getElementById('table1'), {origin: -1});
-        create_gap_rows(ws, 1); // one row gap after first table
-    
-        /* second table */
-        XLSX.utils.sheet_add_aoa(ws, [[this.headers[1]]], {origin: -1});
-        XLSX.utils.sheet_add_dom(ws, document.getElementById('table2'), {origin: -1});
-        create_gap_rows(ws, 2); // two rows gap after second table
-    
-        /* third table */
-        XLSX.utils.sheet_add_aoa(ws, [[this.headers[2]]], {origin: -1});
-        XLSX.utils.sheet_add_dom(ws, document.getElementById('table3'), {origin: -1});
-    
-        /* create workbook and export */
-        const wb = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, "Hoja1");
-        XLSX.writeFile(wb, "Comparativo Planta de Personal.xlsx");
-      };
-    ;
-  
+    /* primera tabla */
+    const ws = XLSX.utils.aoa_to_sheet([[this.headers[0]]]);
+    XLSX.utils.sheet_add_dom(ws, document.getElementById('table1'), {
+      origin: -1,
+    });
+    create_gap_rows(ws, 1); // espacio de una fila después de la primera tabla
 
+    /* segunda tabla */
+    XLSX.utils.sheet_add_aoa(ws, [[this.headers[1]]], { origin: -1 });
+    XLSX.utils.sheet_add_dom(ws, document.getElementById('table2'), {
+      origin: -1,
+    });
+    create_gap_rows(ws, 2); // espacio de dos filas después de la segunda tabla
+
+    /* tercera tabla */
+    XLSX.utils.sheet_add_aoa(ws, [[this.headers[2]]], { origin: -1 });
+    XLSX.utils.sheet_add_dom(ws, document.getElementById('table3'), {
+      origin: -1,
+    });
+
+    /* creación del libro y exportación */
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Hoja1');
+    XLSX.writeFile(wb, 'Comparativo Planta de Personal.xlsx');
+  }
 }
