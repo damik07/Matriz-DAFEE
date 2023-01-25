bb83afc5cbd17511907bae16808ea0a552faf008
damik07 <96784561+damik07@users.noreply.github.com>
2023-01-25 09:38:07 -0300

    1 file modified

--- src/app/Componentes/plantaPersonal/vistas/pl-planilla1-rf/pl-planilla1-rf.component.ts
+++ src/app/Componentes/plantaPersonal/vistas/pl-planilla1-rf/pl-planilla1-rf.component.ts
@@ -17,4 +17,23 @@ export class PlPlanilla1RFComponent implements OnInit {
   ngOnInit() {
   }
 
+  filterData1(formData) {
+    // Filtrar los datos del servicio según el rango de fechas especificado en el formulario
+    const datosFiltrados1 = this.dataPlanta.filter(
+      (dato) =>
+        new Date(dato.fecha).getMonth() ===
+        new Date(formData.startDate1).getMonth()
+    );
+    const dfiltro1 = datosFiltrados1.filter(
+      (dato) =>
+        new Date(dato.fecha).getFullYear() ===
+        new Date(formData.startDate1).getFullYear()
+    );
+
+
+
+    //FIN DE FILTRO
+  };
+
+
 }
